a675e386281b9beaf45cfa8a57476acfc6ff6070
Adham Badr <adhambadr2@gmail.com>
2019-08-19 19:15:38 +0200

    Basic harmless version

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 import _ from 'lodash'
+import fs from 'fs'
+import path from 'path'
 
-_.map(_.range(10),console.log)
+const threshold = 1; // Month
+let saved = 0;
+let counter = 0;
+let searchCounter = 0;
+const checkNodejs = (stats)=>{
+	const {ctime , birthtime , mtime } = stats
+	const d = new Date(mtime)
+	const lapsed = new Date().getTime() - d.getTime()
+	const months = lapsed / 1000 / 60 / 60 / 24 / 30 
+	
+	return months > 1
+}
+
+const deleteFolder = ({ folder ,stats})=>{
+	console.log(" Deleting " , folder);
+	saved += stats.size
+	counter ++;
+}
+
+const cleanDirectory =  ({rootDirectory = ".", verbose = false})=>{
+	searchCounter++;
+	const folder = path.resolve(rootDirectory)
+	if(verbose)
+		console.log("Checking ",folder)
+
+	_.map(fs.readdirSync(rootDirectory) , file=>{
+		const stats = fs.lstatSync(path.join(folder,file))
+		if(stats.isDirectory())
+		{
+			const current = path.join(folder , file)
+			if(file === 'node_modules')
+			{
+				if(checkNodejs(stats))
+					deleteFolder({
+						stats , folder : current 
+					})
+				
+			}
+			else
+				cleanDirectory({rootDirectory : current})
+		}
+	})
+}
+
+cleanDirectory({});
+console.log("Searched",searchCounter," olders")
+console.log("Saved "+counter+" folder",saved , saved/1000000.0 + "MB")
